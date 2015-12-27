42d5184a29a5eaaf581595ca3279e0bafb34a6e5
Nadia <nadiadaliavska@gmail.com>
2015-12-27 12:51:51 +0100

    inject to app.js 'ngRoute' and configure it, to controllers inject 'ngParameters',  add  id to dish

--- conFusionAngular/app/scripts/servises.js
+++ conFusionAngular/app/scripts/servises.js
@@ -8,6 +8,7 @@ angular.module('confusionApp')
         var menuFactory = {};
         var dishes = [
             {
+                _id:0,
                 name: 'Uthapizza',
                 image: 'images/uthapizza.png',
                 category: 'mains',
@@ -49,31 +50,90 @@ angular.module('confusionApp')
                 ]
             },
             {
+                _id:1,
                 name: 'Zucchipakoda',
                 image: 'images/zucchipakoda.png',
                 category: 'appetizer',
                 label: '',
                 price: '1.99',
                 description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-                comments: ''
+                comments: [
+                    {
+                        rating: 5,
+                        comment: "Imagine all the eatables, living in conFusion!",
+                        author: "John Lemon",
+                        date: "2012-10-16T17:57:28.556094Z"
+                    },
+                    {
+                        rating: 4,
+                        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                        author: "Paul McVites",
+                        date: "2014-09-05T17:57:28.556094Z"
+                    },
+                    {
+                        rating: 3,
+                        comment: "Eat it, just eat it!",
+                        author: "Michael Jaikishan",
+                        date: "2015-02-13T17:57:28.556094Z"
+                    }]
             },
             {
+
+                _id:2,
                 name: 'Vadonut',
                 image: 'images/vadonut.png',
                 category: 'appetizer',
                 label: 'New',
                 price: '1.99',
                 description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
-                comments: ''
+                comments: [
+                    {
+                        rating: 5,
+                        comment: "Imagine all the eatables, living in conFusion!",
+                        author: "John Lemon",
+                        date: "2012-10-16T17:57:28.556094Z"
+                    },
+                    {
+                        rating: 4,
+                        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                        author: "Paul McVites",
+                        date: "2014-09-05T17:57:28.556094Z"
+                    },
+                    {
+                        rating: 3,
+                        comment: "Eat it, just eat it!",
+                        author: "Michael Jaikishan",
+                        date: "2015-02-13T17:57:28.556094Z"
+                    }]
             },
             {
+
+                _id:3,
                 name: 'ElaiCheese Cake',
                 image: 'images/elaicheesecake.png',
                 category: 'dessert',
                 label: '',
                 price: '2.99',
                 description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-                comments: ''
+                comments: [
+                    {
+                        rating: 5,
+                        comment: "Imagine all the eatables, living in conFusion!",
+                        author: "John Lemon",
+                        date: "2012-10-16T17:57:28.556094Z"
+                    },
+                    {
+                        rating: 4,
+                        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                        author: "Paul McVites",
+                        date: "2014-09-05T17:57:28.556094Z"
+                    },
+                    {
+                        rating: 3,
+                        comment: "Eat it, just eat it!",
+                        author: "Michael Jaikishan",
+                        date: "2015-02-13T17:57:28.556094Z"
+                    }]
             }];
         menuFactory.getDishes = function(){
             return dishes;
